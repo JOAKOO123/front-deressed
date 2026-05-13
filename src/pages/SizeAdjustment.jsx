037b9dddf7cf97df49c14191fcdfd5f9489afa44
joakoo123 <beltranlive9@gmail.com>
2026-05-13 15:06:14 -0400

    fix: import src/oages/sizeadjunstment.jsx error tipeo

--- src/pages/SizeAdjustment.jsx
+++ src/pages/SizeAdjustment.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-request";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { useAuth } from "../hooks/useAuth";
